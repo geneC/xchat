3fc83cfb22061b3c3bcdd791a85564f36c0c1e98
Gene Cumm <gene.cumm@gmail.com>
2012-08-20 23:54:32 -0400

    privmsg2rcvr.pl: open privmsg /query window
    Signed-off-by: Gene Cumm <gene.cumm@gmail.com>

--- privmsg2rcvr.pl
+++ privmsg2rcvr.pl
@@ -1,35 +1,31 @@
 #!/usr/bin/perl
+# Copyright 2012 Gene Cumm
 
-Xchat::register('privmsg2rcvr','0.030','Move Privmsg messages to the reciever\'s window');
+Xchat::register('privmsg2rcvr','0.04','Move Privmsg messages to the reciever\'s window');
+# Don't eat the message UNLESS we need to move it and minimize how much logic
+# is processed before aborting.
 
 Xchat::hook_server('PRIVMSG',\&PrivMsgToReceiverWin);
+
+# DO try to pop open the desired context window when not found
+use constant PrivMsgToReceiverWinPOP => 1;
 
 sub PrivMsgToReceiverWin {
 	my @msg = @{$_[0]};
 	my @msge = @{$_[1]};
 	my $chan = $msg[2];
-# 	if ( ((substr($chan, 0, 1)) ne "#") && ($chan ne $nick)) {
 	if ( (substr($chan, 0, 1)) ne "#") {
 	  my $nick = Xchat::get_info("nick");
-	  my $omsg = sprintf("%s|%s|%s|%s|%s|%s|%s", $nick, $msg[0],$msg[1],$msg[2],$msg[3],$msg[4], ($msg[5]?$msge[5]:''));
-# 	  Xchat::emit_print("Generic Message","-dbg-".$chan."-", $omsg, null);
-	  if (($chan ne $nick) && (1)) {
-		my $srv = Xchat::get_info("server");
-
-# 		my $chnWin = Xchat::find_context($chan, $srv);
-# 		my $omsg = sprintf("%s|%s|%s|%s|%s|%s|%s", $nick, $msg[0],$msg[1],$msg[2],$msg[3],$msg[4], ($msg[5]?$msge[5]:''));
-
-		Xchat::set_context($chan, $srv);
-
-# 		Xchat::emit_print("Generic Message","-G\00310-\002-\002\003", $omsg, null);
-
-# 		Xchat::emit_print("Generic Message", $nick, $msge[3], null);
+	  if ($chan ne $nick) {
+	    my $srv = Xchat::get_info("server");
+	    if (!(Xchat::find_context($chan, $srv))) {
+		# Attempt to open the message window first
+		Xchat::command("QUERY ".$chan);
+	    }
+	    if (Xchat::set_context($chan, $srv)) {
 		Xchat::emit_print("Channel Message", $nick, substr($msge[3], 1));
-
-# 		Xchat::emit_print("Generic Message", "-".$nick, $msge[3], null);
-# 		Xchat::print($omsg, $chan, $srv);
-
-# 		return Xchat::EAT_ALL;
+		return Xchat::EAT_ALL;
+	    }
 	  }
 	}
 	return Xchat::EAT_NONE;
