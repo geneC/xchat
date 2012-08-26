5d8507a3a85c921384d5a4e2c03f4072d7e80d14
Gene Cumm <gene.cumm@gmail.com>
2012-08-26 13:24:55 -0400

    privmsg2rvcr.pl: remove debug subs
    Signed-off-by: Gene Cumm <gene.cumm@gmail.com>

--- privmsg2rcvr.pl
+++ privmsg2rcvr.pl
@@ -22,7 +22,7 @@
 ##   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 ##   OTHER DEALINGS IN THE SOFTWARE.
 
-Xchat::register('privmsg2rcvr','0.05','Move Privmsg messages to the reciever\'s window');
+Xchat::register('privmsg2rcvr','0.06','Move Privmsg messages to the reciever\'s window');
 # Don't eat the message UNLESS we need to move it and minimize how much logic
 # is processed before aborting.
 
@@ -30,18 +30,6 @@ Xchat::hook_server('PRIVMSG',\&PrivMsgToReceiverWin);
 
 # DO try to pop open the desired context window when not found
 use constant PrivMsgToReceiverWinPOP => 1;
-
-sub PrivMsgToReceiverWinDebugMsgSN {
-	my $cxt = Xchat::get_context();
-	Xchat::command("QUERY "."pvmsg");
-	Xchat::set_context("pvmsg", $_[0]);
-	Xchat::emit_print("Channel Message", $_[1], $_[2]);
-	Xchat::set_context($cxt);
-}
-
-sub PrivMsgToReceiverWinDebugMsg {
-	return PrivMsgToReceiverWinDebugMsgSN(Xchat::get_info("server"), Xchat::get_info("nick"), $_[0]);
-}
 
 sub PrefixToNick {
 	if (($_[0] =~ s/:([^!@]+)([!@].*)?/$1/) >= 1) {
